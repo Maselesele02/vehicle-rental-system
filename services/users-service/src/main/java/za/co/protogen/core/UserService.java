7bbb422023ef01c565082143a191f118dac44a53
Maselesele02 <143250883+Maselesele02@users.noreply.github.com>
2024-02-16 14:44:51 +0200

    added java code to users-service files

---
+++ services/users-service/src/main/java/za/co/protogen/core/UserService.java
@@ -0,0 +1,15 @@
+
+package za.co.protogen.core;
+
+import za.co.protogen.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+    void removeUser(long id);
+    User getUserById(long id);
+    List<User> getAllUsers();
+    void updateUser(long id, User updatedUser);
+    List<User> searchUsers(String firstName, String lastName, String rsaId);
+}
